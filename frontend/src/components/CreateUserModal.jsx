c17169557b485c2af33474e09eb3788574bca5d5
Yingping Ma <yingping.ma2000@gmail.com>
2025-03-09 10:06:20 +0100

    add the ui design

---
+++ frontend/src/components/CreateUserModal.jsx
@@ -0,0 +1,72 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { BiAddToQueue } from "react-icons/bi";
+
+const CreateUserModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>
+        <BiAddToQueue size={20} />
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>My new BFF</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Flex alignItems={"center"} gap={4}>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input placeholder="John Doe" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Input placeholder="Software Engineer" />
+              </FormControl>
+            </Flex>
+            <FormControl>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                resize={"none"}
+                overflowY={"hidden"}
+                placeholder="He's a software engineer who loves to code and build things. "
+              />
+            </FormControl>
+            <RadioGroup defaultValue="female" mt={4}>
+              <Flex gap={5}>
+                <Radio value="female">Female</Radio>
+                <Radio value="male">Male</Radio>
+              </Flex>
+            </RadioGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Add
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default CreateUserModal;
